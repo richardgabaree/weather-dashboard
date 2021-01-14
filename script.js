7ec6081ce62a1ea46ffe6d593122f6ef0fc0d62f
Richard Gabaree <richard.gabaree@gmail.com>
2021-01-13 22:00:39 -0600

    added search results for city, temp, humidity, wind speed and uvIndex

--- script.js
+++ script.js
@@ -6,17 +6,15 @@ $("#search-btn").on("click", function() {
 })
 
 function generateWeather () {
-    var city = $(".form-control").val();
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    var citySearch = $(".form-control").val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIKey;
     $.ajax({
         url: queryURL,
         method: "GET"
       })
         // We store all of the retrieved data inside of an object called "response"
         .then(function(response) {
-
           // Log the resulting object
-          console.log(response);
             var current = response.main.temp
             var city = response.main.name
             var wind = resopnse.main.wind
@@ -26,21 +24,21 @@ function generateWeather () {
 
     var forecastURL = `https://api.openweathermap.org/data/2.5/onecal?lat=${latitude}&lon=${longtitude}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${APIKey}`;
 
-          // Transfer content to HTML
-        //   $(".city").html("<h1>" + response.name + date ,"</h1>");
-        //   $(".wind").text("Wind Speed: " + response.wind.speed);
-        //   $(".humidity").text("Humidity: " + response.main.humidity);
-        //   $(".uvindex").text("UV Index: " + response.main.uvindex);
+          //Transfer content to HTML;
+          $(".city").html("<h1>" + response.name + date ,"</h1>");
+          $(".wind").text("Wind Speed: " + response.wind.speed);
+          $(".humidity").text("Humidity: " + response.main.humidity);
+          $(".uvindex").text("UV Index: " + response.main.uvindex);
         
-        //   // add temp content to html
-        //   $(".temp").text("Temperature (K) " + response.main.temp);
-        //   $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+          // add temp content to html
+          $(".temp").text("Temperature (K) " + response.main.temp);
+          $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
     
-        //   // Log the data in the console as well
-        //   console.log("Wind Speed: " + response.wind.speed);
-        //   console.log("Humidity: " + response.main.humidity);
-        //   console.log("Temperature (F): " + tempF);
-        //   console.log("UV Index: " + response.main.uindex);
+          // Log the data in the console as well
+          console.log("Wind Speed: " + response.wind.speed);
+          console.log("Humidity: " + response.main.humidity);
+          console.log("Temperature (F): " + tempF);
+          console.log("UV Index: " + response.main.uindex);
         });
 }
 })
