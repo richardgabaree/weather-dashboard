de3aba10a82451883d13444d3d1bdf483ef69c34
Richard Gabaree <richard.gabaree@gmail.com>
2021-01-13 22:53:09 -0600

    added css to the 5-day forcast

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ $("#search-btn").on("click", function() {
 function generateWeather () {
     var citySearch = $(".form-control").val();
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIKey;
+    console.log(queryURL)
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -22,7 +23,21 @@ function generateWeather () {
             var longtitude = response.coord.lon
             var latitude = response.coord.lat
 
-    var forecastURL = `https://api.openweathermap.org/data/2.5/onecal?lat=${latitude}&lon=${longtitude}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${APIKey}`;
+            cityLog.push(response.name)
+            localStorage.setItem("cityName", cityLog)
+
+            $("#sideBar").append('<button id =" '+ response.name +' "class = "cityHistory")></button><br>');
+            $("#" + response.name).text(response.name);
+            $(".city").text("City: " + response.name)
+            //change temperature to Imperial
+            $(".temp").text("Temperature F: " + response.main.temp)
+            $(".humidity").text("Humidity: " + response.main.humidity)
+            $(".wind speed").text("Wind Speed: " + response.wind.speed)
+
+    
+    
+    
+            var forecastURL = `https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" longtitude + &exclude=current,minutely,hourly,alerts&units=imperial&appid=${APIKey}`;
 
           //Transfer content to HTML;
           $(".city").html("<h1>" + response.name + date ,"</h1>");
@@ -33,6 +48,11 @@ function generateWeather () {
           // add temp content to html
           $(".temp").text("Temperature (K) " + response.main.temp);
           $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+
+          var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+        // add temp content to html
+        
     
           // Log the data in the console as well
           console.log("Wind Speed: " + response.wind.speed);
